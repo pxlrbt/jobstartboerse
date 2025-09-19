8085bef7c581383758598080b926d52c9bea78df
Dennis Koch <info@pixelarbeit.de>
2025-09-19 18:49:28 +0200

    Add passwort reset

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->passwordReset()
             ->colors([
                 'primary' => Color::Amber,
             ])
