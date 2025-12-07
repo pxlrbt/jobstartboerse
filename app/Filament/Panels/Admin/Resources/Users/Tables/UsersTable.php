df3b0bbbf7188f10c5f2fd23c876cc0938abc739
Dennis Koch <info@pixelarbeit.de>
2025-12-08 00:23:15 +0100

    UserTable: Add send_credentials action

--- app/Filament/Panels/Admin/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Panels/Admin/Resources/Users/Tables/UsersTable.php
@@ -3,13 +3,20 @@
 namespace App\Filament\Panels\Admin\Resources\Users\Tables;
 
 use App\Enums\Role;
+use App\Models\User;
+use Filafly\Icons\Phosphor\Enums\Phosphor;
+use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Auth\Notifications\ResetPassword as ResetPasswordNotification;
 use Filament\Facades\Filament;
+use Filament\Notifications\Notification;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
+use Illuminate\Contracts\Auth\CanResetPassword;
+use Illuminate\Support\Facades\Password;
 use STS\FilamentImpersonate\Actions\Impersonate;
 
 class UsersTable
@@ -42,6 +49,38 @@ class UsersTable
             ])
             ->recordActions([
                 EditAction::make(),
+
+                Action::make('send_credentials')
+                    ->label('Zugangsdaten versenden')
+                    ->icon(Phosphor::FingerprintDuotone)
+                    ->requiresConfirmation()
+                    ->action(function (User $user) {
+                        $status = Password::broker(Filament::getAuthPasswordBroker())->sendResetLink(
+                            ['email' => $user->email],
+                            function (CanResetPassword $user, string $token): void {
+                                $notification = app(ResetPasswordNotification::class, ['token' => $token]);
+                                $notification->url = Filament::getResetPasswordUrl($token, $user);
+
+                                $user->notify($notification);
+                            },
+                        );
+
+                        if ($status !== Password::RESET_LINK_SENT) {
+                            Notification::make()
+                                ->title(__($status))
+                                ->danger()
+                                ->send();
+
+                            return;
+                        }
+
+                        Notification::make()
+                            ->title(__($status))
+                            ->body(($status === Password::RESET_LINK_SENT) ? 'Ein Link zum Zurücksetzen der Zugangsdaten wurde verschickt' : null)
+                            ->success()
+                            ->send();
+                    }),
+
                 Impersonate::make()
                     ->redirectTo(Filament::getPanel('exhibitor')->getUrl()),
             ])
